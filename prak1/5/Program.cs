6cad46b351db5103d91e84502d2b02eb868e4a1d
aniska31 <Avstralia08@mail.ru>
2021-03-03 23:28:43 +0300

    1

--- prak1/5/Program.cs
+++ prak1/5/Program.cs
@@ -4,6 +4,9 @@
 Разархивировать файл и вывести данные о нем
 Удалить файл и архив*/
 using System;
+using System.IO;
+//using System.IO.Compression;
+using  Ionic.Zip;
 
 namespace _5
 {
@@ -11,7 +14,31 @@ namespace _5
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+
+      //Создать архив в форматер zip
+      DirectoryInfo dirInfo = new DirectoryInfo(@"test");
+      if (!dirInfo.Exists)
+      {
+        dirInfo.Create();
+      }
+       Console.WriteLine("Введите название архива:");
+       string nameZip = Console.ReadLine();
+       nameZip += ".rar";
+       ZipFile zip = new ZipFile(@"test\"+ nameZip);
+       zip.Save();
+       Console.WriteLine("Архив был создан\n");
+
+       //Добавить файл в архив
+       Console.WriteLine("Введите название файла,который хотите внести в архив:");
+       string subzip = Console.ReadLine();
+       File.Open(@"test\"+subzip, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+       zip.AddFile(@"test\" + subzip,"");
+       zip.Save();
+       Console.WriteLine("Файл был добавлен в архив.");
+
+      //Разархивировать файл и вывести данные о нем
+      zip.ExtractAll(@"test\more test\");
+      Console.WriteLine("Разархивация прошла успешно:");
     }
   }
 }
