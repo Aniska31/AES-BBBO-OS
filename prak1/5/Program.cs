087f96e9553aac44931b6e3ac46982b3d78f5998
aniska31 <Avstralia08@mail.ru>
2021-03-04 20:12:33 +0300

    done

--- prak1/5/Program.cs
+++ prak1/5/Program.cs
@@ -27,14 +27,17 @@ namespace _5
        ZipFile zip = new ZipFile(@"test\"+ nameZip);
        zip.Save();
        Console.WriteLine("Архив был создан\n");
+       Console.ReadKey();
 
-       //Добавить файл в архив
-       Console.WriteLine("Введите название файла,который хотите внести в архив:");
+      //Добавить файл в архив
+      Console.WriteLine("Введите название файла,который хотите внести в архив:");
        string subzip = Console.ReadLine();
-       File.Open(@"test\"+subzip, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+       FileStream fs= File.Create(@"test\" + subzip);
+       fs.Close();
        zip.AddFile(@"test\" + subzip,"");
        zip.Save();
        Console.WriteLine("Файл был добавлен в архив.\n");
+       Console.ReadKey();
 
       //Разархивировать файл и вывести данные о нем
       zip.ExtractAll(@"test\more test\");
@@ -42,11 +45,24 @@ namespace _5
       DirectoryInfo di = new DirectoryInfo(@"test\more test\");
       FileInfo[] fiArr = di.GetFiles();
       foreach (FileInfo f in fiArr)
-        Console.WriteLine($"Разархивированный файл:\nИмя:{f.Name}\nРазмер:{f.Length}\nНахождение:{f.FullName}");
+        Console.WriteLine($"Разархивированный файл:\nИмя:{f.Name}\nРазмер:{f.Length}\nНахождение:{f.FullName}\n");
+      Console.ReadKey();
 
 
       //Удалить файл и архив
-
+      Console.WriteLine("Вы точно уверены, что хотите удалить? (1 - yes, 0 - no)");
+      string answer = Console.ReadLine();
+      if (answer != "0")
+      {
+        zip.Dispose();
+        File.Delete(@"test\" + nameZip);
+        File.Delete(@"test\" + subzip);
+        File.Delete($@"test\more test\{subzip}");
+        Console.WriteLine("Удаление произошло успешно.");
+      }
+      else
+        Console.WriteLine("Удаление отменено");
+      Console.ReadKey();
     }
   }
 }
