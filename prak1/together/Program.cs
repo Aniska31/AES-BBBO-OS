279a4305de0db3eb61e29cb8a77e9c33b1625e10
aniska31 <Avstralia08@mail.ru>
2021-03-04 22:00:08 +0300

    start

---
+++ prak1/together/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using Ionic.Zip;
+using System.Xml.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace together
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
